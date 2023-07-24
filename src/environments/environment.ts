4e2c43867b53bde871a59f585ed74447220290b0
Ernest Magriñá <ernest8081@gmail.com>
2023-07-24 22:20:30 +0200

    feat: 🫣 add environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'emm-rick-and-morty',
+    appId: '1:573902521629:web:0573e23ea7189defce4df4',
+    storageBucket: 'emm-rick-and-morty.appspot.com',
+    apiKey: 'AIzaSyCxFVkgORaWzd3P8k0a7ora0Ow0rfUnuis',
+    authDomain: 'emm-rick-and-morty.firebaseapp.com',
+    messagingSenderId: '573902521629',
+  },
+  production: false,
+  apiUrl: 'http://localhost:3000',
+};
